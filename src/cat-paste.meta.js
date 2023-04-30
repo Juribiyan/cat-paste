192a2665f6d0c42b6f5360bd53fe4f84080ccb8c
Juribiyan <smugleaf@ya.ru>
2023-04-30 16:26:42 +0500

    Fix for the damned Greasemonkey

--- src/cat-paste.meta.js
+++ src/cat-paste.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         CatPaste
 // @namespace    https://ochan.ru/gds
-// @version      0.1.0
+// @version      0.1.1
 // @description  Clipboard uploading for Catbox
 // @updateURL    https://juribiyan.github.io/cat-paste/src/cat-paste.meta.js
 // @downloadURL  https://juribiyan.github.io/cat-paste/src/cat-paste.user.js
@@ -9,5 +9,6 @@
 // @match        https://catbox.moe/
 // @match        https://litterbox.catbox.moe/
 // @icon         https://juribiyan.github.io/cat-paste/favicon.ico
-// @grant        none
+// @grant        unsafeWindow
+// @run-at       document-idle
 // ==/UserScript==
--- src/cat-paste.user.js
+++ src/cat-paste.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         CatPaste
 // @namespace    https://ochan.ru/gds
-// @version      0.1.0
+// @version      0.1.1
 // @description  Clipboard uploading for Catbox
 // @updateURL    https://juribiyan.github.io/cat-paste/src/cat-paste.meta.js
 // @downloadURL  https://juribiyan.github.io/cat-paste/src/cat-paste.user.js
@@ -9,14 +9,20 @@
 // @match        https://catbox.moe/
 // @match        https://litterbox.catbox.moe/
 // @icon         https://juribiyan.github.io/cat-paste/favicon.ico
-// @grant        none
+// @grant        unsafeWindow
+// @run-at       document-idle
 // ==/UserScript==
 
-(() => {
-  let form = document.querySelector('#dropzoneUpload')
-  , drop = form?.dropzone?.listeners?.find(l => l?.events && ('drop' in l.events)).events.drop
+(async () => {
+  let form = unsafeWindow.document.querySelector('#dropzoneUpload')
+  , attempts = 0
+  while (!form.dropzone && attempts < 10) {
+    await sleep(1000)
+    attempts++
+  }
+  let drop = ([].find.call(form?.dropzone?.listeners, l => l?.events && ('drop' in l.events))).events.drop
   if (drop) {
-    window.addEventListener('paste', ev => {
+    window.addEventListener.call(unsafeWindow, 'paste', ev => {
       let items = ev.clipboardData?.items
       , files = [];
       if (!items) return;
@@ -26,11 +32,18 @@
           files.push(item.getAsFile())
         }
       }
-      drop({
-        dataTransfer: { files: files },
-        preventDefault: function () {},
-        stopPropagation: function () {}
-      })
+      unsafeWindow.__toDrop = cloneInto({
+        dataTransfer: { files: files }
+      }, unsafeWindow)
+      unsafeWindow.__toDrop["preventDefault"] = exportFunction(function() {}, unsafeWindow.__toDrop)
+      unsafeWindow.__toDrop["stopPropagation"] = exportFunction(function() {}, unsafeWindow.__toDrop)
+      drop(unsafeWindow.__toDrop)
+    })
+  }
+
+  function sleep(ms) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => resolve(), ms)
     })
   }
 })()
